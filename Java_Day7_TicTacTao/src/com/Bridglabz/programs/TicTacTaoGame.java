b47d725e446cb1ea107890ddf0868bb235c26337
Reugito <raosahebdhotreedu@gmail.com>
2021-07-02 18:49:12 +0530

    Adding the getSignToPlay method

--- Java_Day7_TicTacTao/src/com/Bridglabz/programs/TicTacTaoGame.java
+++ Java_Day7_TicTacTao/src/com/Bridglabz/programs/TicTacTaoGame.java
@@ -11,4 +11,31 @@ public class TicTacTaoGame {
 	public TicTacTaoGame() {
 		scanner = new Scanner(System.in);
 	}
+	
+	public String getCharValue() {
+		return scanner.next();
+	}
+	
+	public void getSigntoPlay() {
+		boolean sign = false;
+		do {
+			
+			System.out.println("Select the Sign to play");
+			System.out.println("1. x");
+			System.out.println("2. O");
+			String choice = getCharValue() ;
+			if(choice == "1" || choice == "x") {
+				playersign = "X";
+				compsign = "O";
+			}
+			else if(choice == "2" || choice == "o") {
+				playersign = "O";
+				compsign = "x";
+			}
+			else {
+				sign = true;
+				System.out.println("Invalid Selection");
+			}
+		}while(sign);
+	}
 }
