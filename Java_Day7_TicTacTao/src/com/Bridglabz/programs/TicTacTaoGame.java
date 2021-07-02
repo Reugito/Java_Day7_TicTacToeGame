52c4a83fcc88d3b4bae1f080c260392719d85cf2
Reugito <raosahebdhotreedu@gmail.com>
2021-07-02 18:41:37 +0530

    creating TicTacTao Class

---
+++ Java_Day7_TicTacTao/src/com/Bridglabz/programs/TicTacTaoGame.java
@@ -0,0 +1,14 @@
+package com.Bridglabz.programs;
+
+import java.util.Scanner;
+
+public class TicTacTaoGame {
+	Scanner scanner;
+	static char[] board = new char[10];
+	boolean empty = true;
+	static String playername, computername, playersign, compsign;
+
+	public TicTacTaoGame() {
+		scanner = new Scanner(System.in);
+	}
+}
---
+++ Java_Day7_TicTacTao/src/com/Bridglabz/programs/TicTacTaoMain.java
@@ -0,0 +1,10 @@
+package com.Bridglabz.programs;
+
+public class TicTacTaoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
